2f40a40083e3e682af33ebe2d4a0252261b57be7
stellaruanxy <stellaruanxy@gmail.com>
2024-10-02 20:12:32 +1000

    changed url button

--- src/pages/Detection.js
+++ src/pages/Detection.js
@@ -110,13 +110,13 @@ const Detection = () => {
                                     </p>
                                 </div>
                                 <div className="flex justify-center mt-auto">
-                                    <button className="button-yellow hover:bg-yellow-400 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl"
-                                    style={{ textDecoration: 'none' }} disabled={true}>
-                                        Coming Soon
+                                <Link to="/url" className="text-white bg-green-900 hover:bg-green-600 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl"
+                                    style={{ textDecoration: 'none' }}>
+                                        Learn More
                                         <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
                                         </svg>
-                                    </button>
+                                    </Link>
                                 </div>
 
                             </div>                   
