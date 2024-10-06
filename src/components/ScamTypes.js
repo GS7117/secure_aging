e937b896eeaebe3d0ea4207a28ee815cb15ef800
gset0002 <gset0002@student.monash.edu>
2024-10-07 03:30:46 +1100

    removed underline from buttons throughout the website

--- src/components/ScamTypes.js
+++ src/components/ScamTypes.js
@@ -43,7 +43,7 @@ const ScamTypes = () => {
                         <div>
                             <h3 className="text-xl font-semibold">{scam.title}</h3>
                             <p className="text-gray-700 mb-4">{scam.description}</p>
-                            <Link to={scam.link} className="text-white bg-green-900 hover:bg-green-700 inline-flex items-center justify-center px-4 py-2 text-lg shadow-xl rounded-2xl">
+                            <Link to={scam.link} className="text-white bg-green-900 hover:bg-green-700 inline-flex items-center justify-center px-4 py-2 text-lg shadow-xl rounded-2xl" style={{ textDecoration: 'none' }}>
                                 Learn More
                             </Link>
                         </div>
